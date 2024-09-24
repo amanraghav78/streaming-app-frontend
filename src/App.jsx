7c76c3ef90679cd422770ba44129780bca0a22a8
Aman <amanraghav682@gmail.com>
2024-09-24 23:42:12 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import VideoUpload from './components/VideoUpload'
+
+function App() {
+
+  return (
+    <>
+      <div className='flex flex-col items-center justify-center space-y-3 py-4'>
+        <h1 className='text-3xl font-bold text-gray-500'>Welcome to video streaming app!</h1>
+        <VideoUpload/>
+      </div>
+    </>
+  )
+}
+
+export default App
