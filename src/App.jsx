3a592dba1c6185307b29354242cb374e1fe28c14
Aman <amanraghav682@gmail.com>
2024-09-25 00:00:52 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-      <div className='flex flex-col items-center justify-center space-y-3 py-4'>
+      <div className='flex flex-col items-center justify-center space-y-3 py-5'>
         <h1 className='text-3xl font-bold text-gray-500'>Welcome to video streaming app!</h1>
         <VideoUpload/>
       </div>
